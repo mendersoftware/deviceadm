53cd60d642f5bdb975077613424ca63bfb8acc83
Maciej Borzecki <maciej.borzecki@open-rnd.pl>
2016-04-28 14:40:02 +0200

    routing: add stubbed API endpoint routing setup
    Signed-off-by: Maciej Borzecki <maciej.borzecki@open-rnd.pl>

---
+++ routing.go
@@ -0,0 +1,32 @@
+// Copyright 2016 Mender Software AS
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+package main
+
+import (
+	"github.com/ant0ine/go-json-rest/rest"
+	"github.com/pkg/errors"
+)
+
+// create rest.App instance with routing set up for Device Admision
+// Service
+func MakeDevAdmApp() (rest.App, error) {
+	app, err := rest.MakeRouter(
+	// setup routing
+	)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create router")
+	}
+
+	return app, nil
+}
