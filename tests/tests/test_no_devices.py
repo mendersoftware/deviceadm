882dd98edf9fe6bfacddc4181f4897e7304b5fce
Greg Di Stefano <greg.distefano+github@gmail.com>
2016-10-07 15:43:29 +0200

    added api testing
    Signed-off-by: Greg Di Stefano <greg.distefano+github@gmail.com>

---
+++ tests/tests/test_no_devices.py
@@ -0,0 +1,52 @@
+from client import Client
+import bravado
+from common import create_devices
+import pytest
+
+
+class TestPrebootstrap(Client):
+
+
+    def test_get_all_devices_zero(self, expected_total=0):
+        """
+            Test no devices are returned when getting devices prebootstrap
+        """
+        assert len(self.get_all_devices()) == expected_total
+
+
+    def test_get_non_existant_device(self):
+        """
+            Test getting a device when non exist results in 404
+        """
+        try:
+            self.client.devices.get_devices_id(id="0c396e0032f2b4367d6abe709c889ced728df1f97eb0c368a41465aa24a89454").result()
+        except bravado.exception.HTTPError, e:
+            assert e.response.status_code == 404
+        else:
+            pytest.fail("Error code 404 not returned")
+
+
+    def test_get_non_existant_device_status(self):
+        """
+            Test getting a device status when non exist results in 404
+        """
+        try:
+            self.client.devices.get_devices_id_status(id="ac396e0032f2b4367d6abe709c889ced728df1f97eb0c368a41465aa24a89454").result()
+        except bravado.exception.HTTPError, e:
+            assert e.response.status_code == 404
+        else:
+            pytest.fail("Error code 404 not returned")
+
+
+    def test_put_non_existant_device_status_change(self):
+        """
+            Test setting a device status when device does not exist results in 404
+        """
+        try:
+            Status = self.client.get_model('Status')
+            s = Status(status="accepted")
+            self.client.devices.put_devices_id_status(id="ac396e0032f2b4367d6abe709c889ced728df1f97eb0c368a41465aa24a89454", status=s).result()
+        except bravado.exception.HTTPError, e:
+            assert e.response.status_code == 404
+        else:
+            pytest.fail("Error code 404 not returned")
