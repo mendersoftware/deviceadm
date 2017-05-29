68d686df456ea3fec3ad28c5e6485c450299f31d
mchalczynski <marcin.chalczynski@rndity.com>
2017-05-29 11:02:08 +0200

    tests/test_get_devices: multitenant scenario
    Issues: MEN-1074
    
    Changelog: None
    
    Signed-off-by: mchalczynski <marcin.chalczynski@rndity.com>

--- tests/tests/test_get_devices.py
+++ tests/tests/test_get_devices.py
@@ -1,28 +1,25 @@
 from client import ManagementClient
 import bravado
-from common import create_devices
+from common import create_devices, create_devices_mt
 import pytest
 
 @pytest.mark.usefixtures("create_devices")
 class TestPrebootstrap(ManagementClient):
-
-    def test_get_all_devices(self, expected_total=pytest.config.getoption("devices")):
+    def do_test_get_all_devices(self, auth, expected_total=pytest.config.getoption("devices")):
         """
             Test all devices appear after being bootstrapped
         """
-        assert len(self.get_all_devices()) >= int(expected_total)
+        assert len(self.get_all_devices(auth=auth)) >= int(expected_total)
 
-
-    def test_get_devices(self):
+    def do_test_get_devices(self, auth):
         """
             Test getting a specific device
         """
-        devices = self.get_all_devices()
+        devices = self.get_all_devices(auth=auth)
         first = devices[0]
         assert first.status == "pending"
 
-
-    def test_get_non_existant_device(self):
+    def do_test_get_non_existant_device(self, auth):
         """
             Test getting a specific device results in 404
         """
@@ -33,3 +30,30 @@ class TestPrebootstrap(ManagementClient):
         else:
             pytest.fail("Error code 404 not returned")
 
+    def test_get_all_devices(self):
+        self.do_test_get_all_devices(auth=self.uauth)
+
+    def test_get_devices(self):
+        self.do_test_get_devices(auth=self.uauth)
+
+    def test_get_non_existant_device(self):
+        self.do_test_get_non_existant_device(auth=self.uauth)
+
+
+@pytest.mark.usefixtures("create_devices_mt")
+class TestPrebootstrapMultitenant(TestPrebootstrap):
+
+    @pytest.mark.parametrize("tenant_id", ["tenant1", "tenant2"])
+    def test_get_all_devices(self, tenant_id):
+        auth = self.make_user_auth("user", tenant_id)
+        self.do_test_get_all_devices(auth=auth)
+
+    @pytest.mark.parametrize("tenant_id", ["tenant1", "tenant2"])
+    def test_get_devices(self, tenant_id):
+        auth = self.make_user_auth("user", tenant_id)
+        self.do_test_get_devices(auth=auth)
+
+    @pytest.mark.parametrize("tenant_id", ["tenant1", "tenant2"])
+    def test_get_non_existant_device(self, tenant_id):
+        auth = self.make_user_auth("user", tenant_id)
+        self.do_test_get_non_existant_device(auth=auth)
