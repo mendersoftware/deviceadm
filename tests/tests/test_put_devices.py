c0ed664f9272bcc7927c6b78b8e3e7644f874f1c
mchalczynski <marcin.chalczynski@rndity.com>
2017-05-29 11:02:05 +0200

    tests/test_put_devices: multitenant scenario
    the test implementation is shared between the single-tenant and multitenant
    test classes.
    
    the MT test func is also parametrized with 2 tenants, meaning it will be called
    twice, once for each tenant.
    
    Issues: MEN-1074
    
    Changelog: None
    
    Signed-off-by: mchalczynski <marcin.chalczynski@rndity.com>

--- tests/tests/test_put_devices.py
+++ tests/tests/test_put_devices.py
@@ -1,6 +1,7 @@
 from client import ManagementClient
 import bravado
-from common import create_devices
+from common import create_devices, create_devices_mt
+from tenantadm import fake_tenantadm
 import pytest
 
 @pytest.mark.usefixtures("create_devices")
@@ -25,41 +26,54 @@ class TestPrebootstrap(ManagementClient):
 
         assert self.client.devices.get_devices_id(id=device_id, _request_options={"headers": auth}).result()[0].status == expected_final
 
-
-    def test_change_status(self):
-        """
-            Test every possible status transition works, invalid and non-specified transitions fail
-        """
-        r, h = self.client.devices.get_devices(_request_options=self.uauth).result()
+    def do_test_change_status(self, auth):
+        r, h = self.client.devices.get_devices(_request_options={"headers": auth}).result()
 
         firstDevice = r[0]
         secondDevice = r[1]
         thirdDevice = r[2]
 
         # go from pending => accepted
-        self.change_status(firstDevice.id, expected_initial="pending", expected_final="accepted")
+        self.change_status(firstDevice.id, expected_initial="pending", expected_final="accepted", auth=auth)
 
         # go from pending => rejected
-        self.change_status(secondDevice.id, expected_initial="pending", expected_final="rejected")
+        self.change_status(secondDevice.id, expected_initial="pending", expected_final="rejected", auth=auth)
 
         # go from rejected => accepted
-        self.change_status(secondDevice.id, expected_initial="rejected", expected_final="accepted")
+        self.change_status(secondDevice.id, expected_initial="rejected", expected_final="accepted", auth=auth)
 
         # go from accepted => rejected
-        self.change_status(firstDevice.id, expected_initial="accepted", expected_final="rejected")
+        self.change_status(firstDevice.id, expected_initial="accepted", expected_final="rejected", auth=auth)
 
         # go from pending => rejected => accepted
-        self.change_status(thirdDevice.id, expected_initial="pending", expected_final="rejected")
-        self.change_status(thirdDevice.id, expected_initial="rejected", expected_final="accepted")
+        self.change_status(thirdDevice.id, expected_initial="pending", expected_final="rejected", auth=auth)
+        self.change_status(thirdDevice.id, expected_initial="rejected", expected_final="accepted", auth=auth)
 
         # go from rejected => pending
-        self.change_status(firstDevice.id, expected_initial="rejected", expected_final="pending", expected_error_code=400)
+        self.change_status(firstDevice.id, expected_initial="rejected", expected_final="pending", expected_error_code=400, auth=auth)
 
         # go from accepted => pending
-        self.change_status(secondDevice.id, expected_initial="accepted", expected_final="pending", expected_error_code=400)
+        self.change_status(secondDevice.id, expected_initial="accepted", expected_final="pending", expected_error_code=400, auth=auth)
 
         # go from accepted => blah
-        self.change_status(secondDevice.id, expected_initial="accepted", expected_final="blah", expected_error_code=400)
+        self.change_status(secondDevice.id, expected_initial="accepted", expected_final="blah", expected_error_code=400, auth=auth)
 
         # device not found
-        self.change_status(secondDevice.id+'1', expected_initial="accepted", expected_final="pending", expected_error_code=404)
+        self.change_status(secondDevice.id+'1', expected_initial="accepted", expected_final="pending", expected_error_code=404, auth=auth)
+
+    def test_change_status(self):
+        """
+            Test every possible status transition works, invalid and non-specified transitions fail
+        """
+        self.do_test_change_status(auth=self.uauth)
+
+@pytest.mark.usefixtures("create_devices_mt")
+class TestPrebootstrapMultitenant(TestPrebootstrap):
+    @pytest.mark.parametrize("tenant_id", ["tenant1", "tenant2"])
+    def test_change_status(self, tenant_id):
+        """
+            Test every possible status transition works, invalid and non-specified transitions fail
+        """
+        auth = self.make_user_auth("user", tenant_id)
+        with fake_tenantadm():
+            self.do_test_change_status(auth)
