2f7d33840e5e5e0359937defec8d1a837e9544af
mchalczynski <marcin.chalczynski@rndity.com>
2018-04-25 13:45:34 +0200

    tests: test GET /devices
    dedicated class for testing GET /devices.
    
    the main task is to test filtering by 'preauthorized' status, but using
    our newer fixtures we can test all statuses.
    
    changelog: none
    
    Signed-off-by: mchalczynski <marcin.chalczynski@rndity.com>

--- tests/tests/test_get_devices.py
+++ tests/tests/test_get_devices.py
@@ -1,6 +1,9 @@
 from client import ManagementClient
 import bravado
-from common import create_devices, create_devices_mt, api_client_mgmt
+from common import create_devices, create_devices_mt, \
+        api_client_mgmt, \
+        init_authsets, init_authsets_mt,\
+        clean_db, mongo
 import pytest
 
 @pytest.mark.usefixtures("create_devices")
@@ -26,3 +29,29 @@ class TestPrebootstrapMultitenant(TestPrebootstrap):
     def test_get_non_existant_device(self, tenant_id):
         auth = self.make_user_auth("user", tenant_id)
         self.do_test_get_non_existant_device(auth=auth)
+
+
+class TestGetDevicesBase:
+    def _test_get_devices(self, init_authsets, api_client_mgmt, auth=None):
+        # get all authsets
+        authsets = api_client_mgmt.get_all_devices(auth=auth)
+        assert len(authsets) == len(init_authsets)
+
+        # check authsets by status, verify for each known status
+        for status in ['accepted', 'rejected', 'pending', 'preauthorized']:
+            init_with_status = [a for a in init_authsets if a['status']==status]
+            current_with_status = api_client_mgmt.get_all_devices(status=status, auth=auth)
+
+            assert len(init_with_status) == len(current_with_status)
+
+
+class TestGetDevices(TestGetDevicesBase):
+    def test_get_devices(self, init_authsets, api_client_mgmt):
+        self._test_get_devices(init_authsets, api_client_mgmt)
+
+
+class TestGetDevicesMultitenant(TestGetDevicesBase):
+    @pytest.mark.parametrize("tenant_id", ["tenant1", "tenant2"])
+    def test_get_devices(self, init_authsets_mt, api_client_mgmt, tenant_id):
+        auth = api_client_mgmt.make_user_auth("user", tenant_id)
+        self._test_get_devices(init_authsets_mt[tenant_id], api_client_mgmt, auth)
