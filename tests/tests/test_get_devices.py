3ae8b54fe87afd37e30905bc3c9d37b4d64bbd81
mchalczynski <marcin.chalczynski@rndity.com>
2018-04-25 13:41:05 +0200

    tests: cleanup TestPrebootstrap
    this class contains some tests for GET /devices. they are of very low quality
    though and the naming is confusing (what does 'TestPrebootstrap' mean?).
    
    they will be moved to a new TestGetDevices class, using new fixtures, so remove them.
    
    only one test is left in current class, probably to be refactored away when convenient.
    
    changelog: none
    
    Signed-off-by: mchalczynski <marcin.chalczynski@rndity.com>

--- tests/tests/test_get_devices.py
+++ tests/tests/test_get_devices.py
@@ -5,20 +5,6 @@ import pytest
 
 @pytest.mark.usefixtures("create_devices")
 class TestPrebootstrap(ManagementClient):
-    def do_test_get_all_devices(self, auth, expected_total=pytest.config.getoption("devices")):
-        """
-            Test all devices appear after being bootstrapped
-        """
-        assert len(self.get_all_devices(auth=auth)) >= int(expected_total)
-
-    def do_test_get_devices(self, auth):
-        """
-            Test getting a specific device
-        """
-        devices = self.get_all_devices(auth=auth)
-        first = devices[0]
-        assert first.status == "pending"
-
     def do_test_get_non_existant_device(self, auth):
         """
             Test getting a specific device results in 404
@@ -30,29 +16,12 @@ class TestPrebootstrap(ManagementClient):
         else:
             pytest.fail("Error code 404 not returned")
 
-    def test_get_all_devices(self):
-        self.do_test_get_all_devices(auth=self.uauth)
-
-    def test_get_devices(self):
-        self.do_test_get_devices(auth=self.uauth)
-
     def test_get_non_existant_device(self):
         self.do_test_get_non_existant_device(auth=self.uauth)
 
 
 @pytest.mark.usefixtures("create_devices_mt")
 class TestPrebootstrapMultitenant(TestPrebootstrap):
-
-    @pytest.mark.parametrize("tenant_id", ["tenant1", "tenant2"])
-    def test_get_all_devices(self, tenant_id):
-        auth = self.make_user_auth("user", tenant_id)
-        self.do_test_get_all_devices(auth=auth)
-
-    @pytest.mark.parametrize("tenant_id", ["tenant1", "tenant2"])
-    def test_get_devices(self, tenant_id):
-        auth = self.make_user_auth("user", tenant_id)
-        self.do_test_get_devices(auth=auth)
-
     @pytest.mark.parametrize("tenant_id", ["tenant1", "tenant2"])
     def test_get_non_existant_device(self, tenant_id):
         auth = self.make_user_auth("user", tenant_id)
