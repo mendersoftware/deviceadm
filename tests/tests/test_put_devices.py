9b31ab67fa50d0507c0834f75b7e3baf04967565
mchalczynski <marcin.chalczynski@rndity.com>
2018-04-25 10:38:01 +0200

    tests/test_put_devices: use deviceauth mocks
    and fix confusing naming - 'id' is the auth set id,
    'device_id' is the device id.
    
    changelog: none
    
    Signed-off-by: mchalczynski <marcin.chalczynski@rndity.com>

--- tests/tests/test_put_devices.py
+++ tests/tests/test_put_devices.py
@@ -3,19 +3,21 @@ import bravado
 from common import create_devices, create_devices_mt, api_client_mgmt
 from tenantadm import fake_tenantadm
 import pytest
+import deviceauth
 
 @pytest.mark.usefixtures("create_devices")
 class TestPrebootstrap(ManagementClient):
 
-    def change_status(self, device_id, expected_initial, expected_final, expected_error_code=None, auth=None):
+    def change_status(self, id, device_id, expected_initial, expected_final, expected_error_code=None, auth=None):
         if auth is None:
             auth = self.uauth
         Status = self.client.get_model('Status')
         s = Status(status=expected_final)
         try:
-            actual_initial = self.client.devices.get_devices_id(id=device_id, _request_options={"headers": auth}).result()[0].status
+            actual_initial = self.client.devices.get_devices_id(id=id, _request_options={"headers": auth}).result()[0].status
             assert actual_initial == expected_initial
-            self.client.devices.put_devices_id_status(id=device_id, status=s, _request_options={"headers": auth}).result()
+            with deviceauth.run_fake_update_authset_status(device_id, id, expected_final, 204):
+                self.client.devices.put_devices_id_status(id=id, status=s, _request_options={"headers": auth}).result()
         except bravado.exception.HTTPError as e:
             assert e.response.status_code == expected_error_code
             return
@@ -24,7 +26,7 @@ class TestPrebootstrap(ManagementClient):
                 pytest.fail("Expected an exception, but didnt get any!")
                 return
 
-        assert self.client.devices.get_devices_id(id=device_id, _request_options={"headers": auth}).result()[0].status == expected_final
+        assert self.client.devices.get_devices_id(id=id, _request_options={"headers": auth}).result()[0].status == expected_final
 
     def do_test_change_status(self, auth):
         r, h = self.client.devices.get_devices(_request_options={"headers": auth}).result()
@@ -34,32 +36,32 @@ class TestPrebootstrap(ManagementClient):
         thirdDevice = r[2]
 
         # go from pending => accepted
-        self.change_status(firstDevice.id, expected_initial="pending", expected_final="accepted", auth=auth)
+        self.change_status(firstDevice.id, firstDevice.device_id, expected_initial="pending", expected_final="accepted", auth=auth)
 
         # go from pending => rejected
-        self.change_status(secondDevice.id, expected_initial="pending", expected_final="rejected", auth=auth)
+        self.change_status(secondDevice.id, secondDevice.device_id, expected_initial="pending", expected_final="rejected", auth=auth)
 
         # go from rejected => accepted
-        self.change_status(secondDevice.id, expected_initial="rejected", expected_final="accepted", auth=auth)
+        self.change_status(secondDevice.id, secondDevice.device_id, expected_initial="rejected", expected_final="accepted", auth=auth)
 
         # go from accepted => rejected
-        self.change_status(firstDevice.id, expected_initial="accepted", expected_final="rejected", auth=auth)
+        self.change_status(firstDevice.id, firstDevice.device_id, expected_initial="accepted", expected_final="rejected", auth=auth)
 
         # go from pending => rejected => accepted
-        self.change_status(thirdDevice.id, expected_initial="pending", expected_final="rejected", auth=auth)
-        self.change_status(thirdDevice.id, expected_initial="rejected", expected_final="accepted", auth=auth)
+        self.change_status(thirdDevice.id, thirdDevice.device_id, expected_initial="pending", expected_final="rejected", auth=auth)
+        self.change_status(thirdDevice.id, thirdDevice.device_id, expected_initial="rejected", expected_final="accepted", auth=auth)
 
         # go from rejected => pending
-        self.change_status(firstDevice.id, expected_initial="rejected", expected_final="pending", expected_error_code=400, auth=auth)
+        self.change_status(firstDevice.id, firstDevice.device_id, expected_initial="rejected", expected_final="pending", expected_error_code=400, auth=auth)
 
         # go from accepted => pending
-        self.change_status(secondDevice.id, expected_initial="accepted", expected_final="pending", expected_error_code=400, auth=auth)
+        self.change_status(secondDevice.id, secondDevice.device_id, expected_initial="accepted", expected_final="pending", expected_error_code=400, auth=auth)
 
         # go from accepted => blah
-        self.change_status(secondDevice.id, expected_initial="accepted", expected_final="blah", expected_error_code=400, auth=auth)
+        self.change_status(secondDevice.id, secondDevice.device_id, expected_initial="accepted", expected_final="blah", expected_error_code=400, auth=auth)
 
         # device not found
-        self.change_status(secondDevice.id+'1', expected_initial="accepted", expected_final="pending", expected_error_code=404, auth=auth)
+        self.change_status(secondDevice.id+'1', secondDevice.device_id, expected_initial="accepted", expected_final="pending", expected_error_code=404, auth=auth)
 
     def test_change_status(self):
         """
